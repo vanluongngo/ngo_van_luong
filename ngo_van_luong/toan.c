00dc27b823711b5cd0b3edaedbddc6e58dcba96e
vanluongngo <vanluongtk97@gmail.com>
2025-03-09 09:54:41 +0700

    thêm file toan.c

---
+++ ngo_van_luong/toan.c
@@ -0,0 +1,28 @@
+//Hàm c?ng//
+int cong(int a, int b)
+{
+	int tong = 0;
+	tong = a + b;
+	return tong;
+}
+//Hàm tr?//
+int tru(int a, int b)
+{
+	int hieu = 0;
+	hieu = a - b;
+	return hieu;
+}
+//Hàm nhân//
+int nhan(int a, int b)
+{
+	int tich = 0;
+	tich = a * b;
+	return tich;
+}
+//Hàm chia//
+int chia(int a, int b)
+{
+	int thuong = 0;
+	thuong = a / b;
+	return thuong;
+}

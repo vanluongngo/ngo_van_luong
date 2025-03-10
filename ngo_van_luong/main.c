949814c8759c39398c173d28839e3895b7e45fd0
vanluongngo <vanluongtk97@gmail.com>
2025-03-10 21:58:35 +0700

    thêm lãi ngân hàng

--- ngo_van_luong/main.c
+++ ngo_van_luong/main.c
@@ -1,20 +1,18 @@
 #include<stdio.h>
-#include"toan.h"
 void main()
 {
-	int n = 0;
-	printf("nhap so dien:");
-	scanf_s("%d", &n);
-	if (n <= 50)
-		printf("so tien phai tra la: %d", bac1(n));
-	else if (n <= 100)
-		printf("so tien phai tra la: %d", bac1(50) + bac2(n-50));
-	else if (n <= 200)
-		printf("so tien phai tra la: %d", bac1(50) + bac2(50) + bac3(n-100));
-	else if (n <= 300)
-		printf("so tien phai tra la: %d", bac1(50) + bac2(50) + bac3(100) + bac4(n-200));
-	else if (n <= 400)
-		printf("so tien phai tra la: %d", bac1(50) + bac2(50) + bac3(100) + bac4(100) + bac5(n-300));
-	else
-		printf("so tien phai tra là: %d", bac1(50) + bac2(50) + bac3(100) + bac4(100) + bac5(100) + bac6(n-400));
+	int tien_gui = 0;
+	float lai_suat = 0;
+	int so_nam = 0;
+	printf("nhap so tien: ");
+	scanf_s("%d", &tien_gui);
+	printf("nhap lai suat moi nam: ");
+	scanf_s("%f", &lai_suat);
+	printf("nhap so nam gui: ");
+	scanf_s("%d", &so_nam);
+	for (int i = 0; i <= so_nam; i++)
+	{
+		tien_gui = tien_gui * (1 + lai_suat);
+	}
+	printf("so tien nhan duoc sau %d la:%d", so_nam, tien_gui);
 }

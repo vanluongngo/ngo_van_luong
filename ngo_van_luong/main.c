4a4ec6eb64dceb6cf91f2bb17a09b5098c706d8e
vanluongngo <vanluongtk97@gmail.com>
2025-03-13 01:31:55 +0700

    vị trí của số lơn nhất trong mảng

--- ngo_van_luong/main.c
+++ ngo_van_luong/main.c
@@ -1,20 +1,19 @@
-#include<stdio.h>
+﻿#include<stdio.h>
 #include"toan.h"
 void main()
 {
-	int tong_so_le = 0;
 	int j = 0;
+	int max = 0;
 	int arr[9] = { 1, 5, 4, 8, 9, 3, 4, 7, 1001 };
 	for (int i = 0; i < 9; i++)
 	{
-		if (arr[i] % 2 == 1)
+		if (arr[i] > max)
 		{
-			tong_so_le += arr[i];
-			j++;
+			max = arr[i];
+			j = i;
 		}
 	}
-	if(j>0)
-		printf("trung binh cong cac so le trong mang la: %d", tong_so_le / j);
-	else
-		printf("mang da cho khong co so le");
-}
+
+	printf("vi tri của so lon nhat trong mang la: %d", j);
+
+};

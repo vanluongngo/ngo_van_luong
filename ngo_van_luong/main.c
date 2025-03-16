1d1c831b983996a9d36595881e403a99376f7d34
vanluongngo <vanluongtk97@gmail.com>
2025-03-16 10:24:36 +0700

    update

--- ngo_van_luong/main.c
+++ ngo_van_luong/main.c
@@ -1,15 +1,11 @@
 ﻿#include<stdio.h>
 void main()
 {
-	char str[] = "Ngo Van Luong";
+	char str[] = "Xin Chao Moi Nguoi ^%$";
 	int i = 0;
 	while (str[i] != 0)
 	{
-		if (str[i] >= 65 && str[i] <= 90)
-		{
-			str[i] = str[i];
-		}
-		else if (str[i] >= 97 && str[i]<=122)
+	if (str[i] >= 97 && str[i]<=122)
 			str[i] = str[i] -32;
 		i++;
 	}

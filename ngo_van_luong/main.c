0426f81f67107f7d7f735b416e9b4bf430292bfe
vanluongngo <vanluongtk97@gmail.com>
2025-03-16 10:18:20 +0700

    sửa

--- ngo_van_luong/main.c
+++ ngo_van_luong/main.c
@@ -4,14 +4,14 @@ void main()
 	char str[] = "Ngo Van Luong";
 	int i = 0;
 	while (str[i] != 0)
-		i++;
 	{
 		if (str[i] >= 65 && str[i] <= 90)
 		{
 			str[i] = str[i];
 		}
-		else
-			str[i] = str[i] + 32;
+		else if (str[i] >= 97 && str[i]<=122)
+			str[i] = str[i] -32;
+		i++;
 	}
 	printf("so ky tu in hoa trong chuoi la: %s", str);
 }

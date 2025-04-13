6924816ecaa35a42d11f85dda36b9035e98bffa6
vanluongngo <vanluongtk97@gmail.com>
2025-04-13 08:34:05 +0700

    chia 2 phân số

--- ngo_van_luong/main.c
+++ ngo_van_luong/main.c
@@ -4,10 +4,10 @@
 
 void main()
 {
-    phan_so_t A = { 5,8 };
-    phan_so_t B = { 2,3 };
+    phan_so_t A = { 10,8 };
+    phan_so_t B = { 2,4 };
     phan_so_t Ket_qua;
-    Ket_qua =NhanPhanSo(A, B);
+    Ket_qua =ChiaPhanSo(A, B);
     printf("Ket qua la:%d/%d", Ket_qua.tu, Ket_qua.mau);
  
 }

315391842dd058f85d768ff1842bd61c746d2fe7
vanluongngo <vanluongtk97@gmail.com>
2025-04-13 08:41:03 +0700

    trừ 2 phân số

--- ngo_van_luong/main.c
+++ ngo_van_luong/main.c
@@ -4,10 +4,10 @@
 
 void main()
 {
-    phan_so_t A = { 1,2 };
+    phan_so_t A = { 3,2 };
     phan_so_t B = { 2,3 };
     phan_so_t Ket_qua;
-    Ket_qua =CongPhanSo(A, B);
+    Ket_qua =TruPhanSo(A, B);
     printf("Ket qua la:%d/%d", Ket_qua.tu, Ket_qua.mau);
  
 }
